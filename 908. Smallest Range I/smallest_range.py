daae0340e97cd9dbebecf91c9ea84078fb63639b
Mike Ward <xenophon.of.athens@gmail.com>
2018-11-04 16:43:59 -0700

    908. Smallest Range I

---
+++ 908. Smallest Range I/smallest_range.py
@@ -0,0 +1,14 @@
+"""
+PROMPT: Given an array A of integers, for each integer A[i] we may choose any x with
+-K <= x <= K, and add x to A[i].After this process, we have some array B. Return the
+smallest possible difference between the maximum value of B and the minimum value of B.
+"""
+class Solution(object):
+    def smallestRangeI(self, A, K):
+        biggest = max(A)
+        smallest = min(A)
+        sk = smallest + K
+        if sk >= biggest - K:
+            return 0
+        else:
+            return biggest - K - sk
